884e525cdefd8c8e52c43d805c274579448819b4
Rasika Weliwita <weliwita@gmail.com>
2017-05-15 06:13:37 +0530

    input in a loop

---
+++ CPP/bjorn_3.5/input.cpp
@@ -0,0 +1,16 @@
+
+#include "std_lib_facilities.h"
+
+int main()
+{
+    string previous = "";
+    string current;
+    while(cin>>current){
+        if(previous == current){
+            cout << "repeated word: " << current << "\n";
+        }
+        previous = current;
+    }
+    keep_window_open();
+    return 0;
+}
